544381d4315c9d7d32dc964c7a15eb7932bd8aaf
Ankitsharma2222 <97334778+Ankitsharma2222@users.noreply.github.com>
2022-02-14 15:26:29 +0530

    Add files via upload

---
+++ assignment 3/asgn3_output_21103021.py
@@ -0,0 +1,82 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+Enter the string: ankit
+The occurance of letter "a" is 1 time
+The occurance of letter "n" is 1 time
+The occurance of letter "k" is 1 time
+The occurance of letter "i" is 1 time
+The occurance of letter "t" is 1 time
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+Enter the day: 29
+Enter the month: 2
+Enter the year: 2000
+The next date is 1/3/2000
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+how many number you want to enter>> 2
+enter the number >> 15
+enter the number >> 25
+output is [(15.0, 225.0), (25.0, 625.0)]
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+Please enter your grade point>> 9
+Your Grade is 'A' and Excellent Performance
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+      
+       
+        
+         
+          
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+Enter 'Y' if you want to enter more data ,else enter 'N' >>> y
+Enter your name :ankit sharma
+Enter your sid : 21103021
+Enter 'Y' if you want to enter more data ,else enter 'N' >>> y
+Enter your name :jonny
+Enter your sid : 21103022
+Enter 'Y' if you want to enter more data ,else enter 'N' >>> y
+Enter your name :ronny
+Enter your sid : 21103020
+Enter 'Y' if you want to enter more data ,else enter 'N' >>> n
+part (a)
+{21103021: 'ankit sharma', 21103022: 'jonny', 21103020: 'ronny'}
+part (b)
+{21103021: 'ankit sharma', 21103022: 'jonny', 21103020: 'ronny'}
+Part (c)
+{21103020: 'ronny', 21103021: 'ankit sharma', 21103022: 'jonny'}
+part(d)
+Please enter a sid which you want to search>>21103021
+your searched student name is >> ankit sharma
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+How many terms you want to print>>6
+0
+1
+1
+2
+3
+5
+the average of above series is  2.0
+
+===================== RESTART: C:/Users/Lenovo/Desktop/1.py ====================
+part (a)>>
+{1, 3, 5, 6, 8}
+Part (b)>>
+{3, 6, 8, 9, 13, 17}
+Part (C)>>
+{1, 2, 4, 5}
+Part (D)>>
+{6, 7, 8, 9, 10}
+Part (E)>>
+{10, 7}
