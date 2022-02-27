cdedbec45754ad343b23237a57f3e9bf6a3467ba
Ankitsharma2222 <97334778+Ankitsharma2222@users.noreply.github.com>
2022-02-27 17:44:59 +0530

    Add files via upload

---
+++ assignment 4/assign4_output_21103021.py
@@ -0,0 +1,61 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+enter the number of disks>>3
+ Move disk 1 from rod A to rod B
+ move disk 2 from rod A to rod C
+ Move disk 1 from rod B to rod C
+ move disk 3 from rod A to rod B
+ Move disk 1 from rod C to rod A
+ move disk 2 from rod C to rod B
+ Move disk 1 from rod A to rod B
+ -----Done----
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+enter the Length of pascal triangle >> 5
+By loop Method
+        1
+       1  1
+      1  2  1
+     1  3  3  1
+    1  4  6  4  1
+Enter the length of pascal triangle>>5
+By Recursion
+1 
+1 1 
+1 2 1 
+1 3 3 1 
+1 4 6 4 1 
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+Enter the 1st number>>5
+Enter the 2nd number >>2
+Part(A)
+Quotient  is callable >> False
+Remainder is callable >> False
+Part(B)
+All values are non zero
+Part(c)
+The values greater than 4 is contained in this list  [6, 5, 7, 6, 8, 7]
+Part(D)
+our set is >> {8, 5, 6, 7}
+Part (E)
+Immutable set is >> frozenset({8, 5, 6, 7})
+Part(F)
+The required hash value of maximum valued element of set is  8
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+Name is ankit sharma and Roll no.  is 21103021
+Your object is destroyed
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+Part(a)
+The new salary of Mehak is 70000
+The employee record of Viren is deleted
+
+================ RESTART: C:/Users/Lenovo/Desktop/fgfhfjjhjhk.py ===============
+George please enter a word>>cup
+Enter the word you want to check >>puc
+Is the word is make any sense (y/n)>>n
+your friendship is fake
